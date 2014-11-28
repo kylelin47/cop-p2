6c7f61a06bb27fe15ef4934e673d22c3699173b2
Kyle Lin <kylelin47@gmail.com>
2014-11-28 16:38:21 -0500

    .

--- part5/cathash.cpp
+++ part5/cathash.cpp
@@ -19,10 +19,17 @@ std::size_t hash( std::string const& s ) {
 
 int main( int argc, char** argv ) {
     std::string line;
+    struct rusage usage;
+    struct timeval start, end;
+    getrusage(RUSAGE_SELF, &usage);
+    start = usage.ru_utime;
     while ( getline( std::cin, line ) )
     {
         hash(line);
     }
-    getrusage();
+    getrusage(RUSAGE_SELF, &usage);
+    end = usage.ru_utime;
+    printf("Started at: %ld.%lds\n", start.tv_sec, start.tv_usec);
+    printf("Ended at: %ld.%lds\n", end.tv_sec, end.tv_usec);
     return 0;
 }
